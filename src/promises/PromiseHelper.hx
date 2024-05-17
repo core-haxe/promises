6dbcc4be064243c476b2ef5ad36aae611673f2f3
Ian Harrigan <ianharrigan@hotmail.com>
2024-05-17 08:23:02 +0200

    custom onWaiting handling

--- src/promises/PromiseHelper.hx
+++ src/promises/PromiseHelper.hx
@@ -5,10 +5,14 @@ class PromiseHelper<T> {
     private var _executed:Bool = false;
     private var _waiting:Array<{resolve: ResolveFunction<T>, reject:RejectFunction}> = [];
     private var _onlyExecuteOnce:Bool = false;
+    private var _id:String;
     private var _result:T;
+    private var _onWaiting:ResolveFunction<T>->RejectFunction->Void;
 
-    public function new(onlyExecuteOnce:Bool = false) {
+    public function new(onlyExecuteOnce:Bool = false, id:String = null, onWaiting:ResolveFunction<T>->RejectFunction->Void = null) {
         _onlyExecuteOnce = onlyExecuteOnce;
+        _id = id;
+        _onWaiting = onWaiting;
     }
 
     public function resetAccess() {
@@ -19,6 +23,8 @@ class PromiseHelper<T> {
         return new Promise((resolve, reject) -> {
             if (_onlyExecuteOnce && _executed) {
                 resolve(_result);
+            } else if (_executing && _onWaiting != null) {
+                _onWaiting(resolve, reject);
             } else if (_executing) {
                 _waiting.push({resolve: resolve, reject: reject});
             } else {
